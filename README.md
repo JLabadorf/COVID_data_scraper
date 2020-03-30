5a11d0e782b759bae3be6d4ce32b0122599699e7
Labadorf <jlabado1@uhhs.com>
2020-03-30 13:16:48 -0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# COVID_data_scraper
- 
+# Ohio COVID-19 Public Data
+*SOURCE:Ohio Department of Health* 
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+
+## PURPOSE
+This API is designed to help data users get publically available information from the Ohio Department of Health. Other states are in the work. Anyone interested in helping develop this, please email james@jameslabadorf.com.
+## API ROUTES
+This is simply an API bridge for the data. All data is pulled when the API request is made. The Ohio Department of Health updates their data at 2:00PM EST. 
+### `'/all/'`
+returns all the data, unfiltered. 
+
+### `'/county/'`
+This route summarizes all data on the county level. This returns the data in two sets, one for Hospitalizations and another for deaths.
+
+### `'/county/<county>/'`
+This API route returns data based on the county queried. replace `<county>` with the county you wish to query.
+
+### `'/sex/'` 
+This API route returns data summerized on the sex of the patient.
+
+### `'/onset/date/'`
+This API route returns the data summerized by date of onset of symptons
+
+### `'/onset/date/<county>'`
+This API route returns the data summerized by date of onset filtered on the queried county. 
+### `'/death/'`
+This API route summarizes the data based on date of death. 
+
+### `'/age/'`
+This API route summarizes the data by age group.
---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import jsonify, Flask, render_template
+import requests
+import pandas as pd
+
+
+app = Flask(__name__,template_folder='.')
+
+oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/all/') # This pulls all the data from the ODH
+def all_data():
+    oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+    df = pd.read_csv(oh_url)
+    print(df.head(10))
+    d  = df.to_dict()
+    return jsonify(d)
+
+@app.route('/county/')
+def county_data():
+    oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+    df = pd.read_csv(oh_url).groupby(['County']).sum()
+    d = df.to_dict()
+    return jsonify(d)
+    
+@app.route('/county/<county>/')
+def county_lookup(county):
+    oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+    df = pd.read_csv(oh_url).set_index(['County']).groupby(['County']).sum()
+    d = df.loc[county].to_dict()
+    return jsonify(d)
+
+@app.route('/sex/')
+def sex():
+    oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+    df = pd.read_csv(oh_url).set_index(['Sex']).groupby(['Sex']).sum()
+    d = df.to_dict()
+    return jsonify(d)
+
+@app.route('/onset/date/')
+def onset_date():
+    oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+    df = pd.read_csv(oh_url).set_index(['Onset Date']).groupby(['Onset Date']).sum()
+    d = df.to_dict()
+    return jsonify(d)
+
+@app.route('/onset/date/<county>/')
+def onset_date_county(county):
+    oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+    df = pd.read_csv(oh_url).set_index(['County']).groupby(['County','Onset Date']).sum().sort_values(['Onset Date'])
+    d = df.loc[county].to_dict()
+    return jsonify(d)
+
+@app.route('/death/')
+def death_count():
+    oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+    df = pd.read_csv(oh_url).set_index(['Date Of Death']).groupby(['Date Of Death']).sum()
+    d = df.to_dict()
+    return jsonify(d)
+
+@app.route('/age/')
+def age_group():
+    oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
+    df = pd.read_csv(oh_url).set_index(['Age Range']).groupby(['Age Range']).sum()
+    d = df.to_dict()
+    return jsonify(d)
