0f8615adcfad22daf1bff135f4603495000383e0
James Labadorf <james@jameslabadorf.com>
2020-05-11 17:42:06 -0400

    updated for deaths

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def summuraized():
 
     df_county = df[['County','Case Count','Death Count','Hospitalized Count']]
     df_county['Case Count'] = df_county['Case Count'].str.replace(',','').astype(int)
-    #df_county['Death Count'] = df_county['Death Count'].str.replace(',','').astype(int)
+    df_county['Death Count'] = df_county['Death Count'].str.replace(',','').astype(int)
     df_county['Hospitalized Count'] = df_county['Hospitalized Count'].str.replace(',','').astype(int)
     df_county = df_county.set_index(['County']).sum()
 
@@ -35,15 +35,51 @@ def all_data():
 
 @app.route('/county/')
 def county_data():
+    param = request.args.get("g")
     oh_url = r'https://coronavirus.ohio.gov/static/COVIDSummaryData.csv'
     df = pd.read_csv(oh_url)
-    df_county = df[['County','Case Count','Death Count','Hospitalized Count']]
+    if param=="d":
+        df_county = df[['County','Case Count','Death Count','Hospitalized Count','Onset Date']]
+    else:
+        df_county = df[['County','Case Count','Death Count','Hospitalized Count']]
     df_county['Case Count'] = df_county['Case Count'].str.replace(',','').astype(int)
-    #df_county['Death Count'] = df_county['Death Count'].str.replace(',','').astype(int)
+    df_county['Death Count'] = df_county['Death Count'].str.replace(',','').astype(int)
     df_county['Hospitalized Count'] = df_county['Hospitalized Count'].str.replace(',','').astype(int)
-    df_county = df_county.set_index(['County']).groupby(['County']).sum()
-
-    d = df_county.to_dict()
+    if param =="d":
+        d={}
+        df_county = df_county.set_index(['Onset Date']).groupby(['County','Onset Date']).sum()
+        counties = []
+        dates=[]
+        for i, rows in df_county.iterrows():
+            counties.append(i[0])
+            dates.append(i[1])
+        county_list = []
+        date_list = []
+        for date in dates:
+            if date not in date_list:
+                date_list.append(date)
+                d[date] ={}
+        
+        for i, rows in df_county.iterrows():
+            date = i[1]
+            county = i[0]
+            d[date][county] = {}
+        #print(d)
+        for i, rows in df_county.iterrows():
+            print(i[1])
+            county = i[0]
+            date = i[1]
+            case_count = rows[0]
+            death_count = rows[1]
+            hosp_count = rows[2]
+            d[date][county] = []
+            d[county].append({"Case Count":case_count,"Death Count":death_count,"Hospitalized Count":hosp_count})
+           
+    else:
+        df_county = df_county.set_index(['County']).groupby(['County']).sum()
+        d = df_county.to_dict()
+    print(d)
+    #print(df_county.head(100))
     return jsonify(d)
     
 @app.route('/county/<county>/')
